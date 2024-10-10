f5b08965b7e8161b31df25c56cc6076f4f54c484
comfort <comfort.prince.hluyo@gmail.com>
2024-10-10 06:23:55 +0200

    Completed the Laravel Auth basics tasks

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -15,6 +18,33 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+
+        if(!Auth::check())
+            to_route('login');
+
+        if($request->password !== null ) {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            ]);
+            
+            Auth::user()->update([
+                'name' => $request->name,
+                'email'=> $request->email,
+                'password'=> Hash::make($request->password),
+            ]);    
+        } else {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            ]);
+
+            Auth::user()->update([
+                'name' => $request->name,
+                'email'=> $request->email
+            ]);
+        }
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
